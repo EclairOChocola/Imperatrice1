0b899a9226666ec694c1737d2e5789c24aecf664
Rémy Dumas <remy-dumas@hotmail.fr>
2016-01-27 14:36:50 +0100

    init

---
+++ app.js
@@ -0,0 +1,185 @@
+﻿// Imports
+var url 				= require('url');
+var params 			= require('params');
+var querystring = require('querystring');
+var express 		= require('express');
+var app 				= express();
+var http 				= require('http');
+var server 			= http.createServer();
+var async 			= require('async');
+var request 		= require('request');
+
+// Var globals
+var nbRequetes = 0;
+var nbConnexions = 0;
+
+app.use(express.static(__dirname + "/libs"));
+
+//================================================================
+//================================================================
+app.get('/', function(req, res) {
+	res.render('accueil.ejs'); //console.log("accueil");
+});
+
+app.get('/sortie', function(req, res) {
+	res.render('sortie.ejs'); //console.log("sortie");
+});
+
+/*app.get('/servicesweb/overpass', function(req, res) {
+	res.render('overpass.ejs'); //console.log("sortie");
+});*/
+
+app.get('/params', function(req, res) {
+	var urlObjet = url.parse(req.url);
+	var chemin = urlObjet.pathname;
+	var paramsTab = querystring.parse(urlObjet.query); //Parse parametres
+	res.send("<h1> Liste des parametres </h1> <br/>" + params.params2html(paramsTab)); //console.log("params");
+});
+
+app.get('/bonjour/:var1/:var2', function(req, res) {
+	res.render('bonjour.ejs', {prenom: req.params.var1, nom:req.params.var2});
+});
+
+app.get('/servicesweb/overpass', function(req, res) {
+	var donnees = "";
+	if(req.query.type) {
+		var valeur = req.query.type;
+	} else {
+		var valeur = "bar";
+	}
+	var url = "http://overpass-api.de/api/interpreter?[out:json];(node[amenity="+valeur+"](45.15414,5.677606,45.214077,5.753118););out;";
+	var reqOverpass = http.get(url, function(resOverpass){
+		resOverpass.on('data', function(data) {
+			donnees += data;
+		});
+
+		resOverpass.on('end', function(){
+			donnees = JSON.parse(donnees);
+			res.render('overpass.ejs', {elements : donnees.elements} );
+		});
+
+	});
+	reqOverpass.end();
+	reqOverpass.on('error', function(e) {
+		console.error(e);
+	});
+});
+
+
+
+app.get('/APIs', function(req, res) {
+	if(req.query.type) {
+		var valeur = req.query.type;
+	} else {
+		var valeur = "bar";
+	}
+	var url1 = "http://overpass-api.de/api/interpreter?[out:json];(node[amenity="+valeur+"](45.15414,5.677606,45.214077,5.753118););out;";
+	var url2 = "http://taginfo.openstreetmap.org/api/4/key/values?key=amenity&page=1&rp=100&sortname=count_all&sortorder=desc";
+
+	async.parallel([
+	function(callback) {
+		request(url1, function(err, response, body) {
+			obj = JSON.parse(body);
+			callback(false, obj);
+		});
+	},
+	function(callback) {
+		request(url2, function(err, response, body) {
+			obj = JSON.parse(body);
+			callback(false, obj);
+		});
+	},],
+	function(err, results) {
+		res.render('APIs.ejs', {api1:results[0].elements, api2:results[1].data} );
+	});
+	//res.render('APIs.ejs');
+});
+
+
+
+app.get('/compte/creation', function(req, res) {
+	if(req.query.pseudo && req.query.mail && req.query.pass) {
+		var pseudo = req.query.pseudo;
+		var mail = req.query.mail
+		var pass = req.query.pass
+		console.log(pseudo + " " + mail + " " + pass);
+
+		var sqlite3 = require('sqlite3').verbose();
+		var db = new sqlite3.Database('mydb.db');
+		db.serialize(function() {
+		db.run('CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT PRIMARY KEY, pseudo varchar(100) not null, mail varchar(100) not null, pass varchar(100) not null)');
+		db.all("SELECT * FROM user", function(err, row) {
+	    console.log(row.id + ": " + row.pseudo);
+		});
+		db.run('INSERT INTO user(pseudo,mail,pass) VALUES ('+ pseudo +','+ mail +','+ pass +')');
+		});
+		db.close();
+	} else {
+	}
+
+	res.render('creation_compte.ejs', {});
+});
+
+//================================================================
+//================================================================
+
+
+
+
+
+
+//================================================================
+//================================================================
+/*var donnees;
+var url = "http://overpass-api.de/api/interpreter?[out:json];%28node[amenity=bar]%2845.15414,5.677606,45.214077,5.753118%29;%29;out;";
+var reqOverpass = http.get(url, function(resOverpass){
+	resOverpass.on('data', function(data) {
+		console.log('Reception de données :\n' + data);
+	});
+
+	resOverpass.on('end', function(res){
+		console.log('La requete est terminee.');
+		//JSON.parse(donnees);
+		res.render('overpass.ejs', {elements: donnees}); //console.log("sortie");
+	});
+});
+reqOverpass.end();
+reqOverpass.on('error', function(e) {
+	console.error(e);
+});*/
+
+
+/*reqOverpass.end();*/
+//================================================================
+//================================================================
+
+
+
+
+
+
+//================================================================
+//================================================================
+app.use(function(req, res) {
+console.log("Page introuvable");
+res.send('La page demandée n\'existe pas');
+});
+
+server.on('request', function(req, res) {
+	res.writeHead(200);
+	nbRequetes ++;
+	console.log("Requete N°"+nbRequetes);
+});
+
+server.on("connection", function () {
+	if (nbConnexions >= 2) {
+		server.close();
+		console.log("Serveur fermé !");
+	} else {
+		nbConnexions ++;
+		console.log("New user !");
+	}
+});
+app.listen(8181);
+//================================================================
+//================================================================
